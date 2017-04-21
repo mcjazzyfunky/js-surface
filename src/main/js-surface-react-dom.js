bf9edfe799e42b8c938fa85475f782339a5d0d00
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2017-04-21 17:07:40 +0200

    Some refactoring

--- src/main/js-surface-react-dom.js
+++ src/main/js-surface-react-dom.js
@@ -1,23 +1,44 @@
+import createExports from './internal/module/createExports.js';
 import defineDependentFunctions from './internal/react/defineDependentFunctions.js';
-import defineClassComponent from './api/defineClassComponent.js';
-import defineDispatchComponent from './api/defineDispatchComponent.js';
-import hyperscript from './api/hyperscript.js';
-import Component from './api/Component.js';
-import Spec from './api/Spec.js';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const {
-    defineStandardComponent,
-    defineFunctionalComponent,
-    isElement
-} = defineDependentFunctions({
+const moduleConfig = defineDependentFunctions({
     Component: React.Component,
     createElement: createElement,
     createFactory: React.createFactory,
     isValidElement: React.isValidElement
 });
+alert(1)
+moduleConfig.createElement = React.createElement;
+
+moduleConfig.render = (content, targetNode) => {
+    if (!isElement(content)) {
+        throw new TypeError(
+            "[render] First argument 'content' has to be a valid element");
+    }
+
+    if (typeof targetNode === 'string') {
+        targetNode = document.getElementById(targetNode);
+    }
+
+    return ReactDOM.render(content, targetNode);
+};
+
+const {
+    createElement,
+    defineDispatchComponent,
+    defineClassComponent,
+    defineFunctionalComponent,
+    defineStandardComponent,
+    hyperscript,
+    isElement,
+    render,
+    Component,
+    Spec,
+} = createExports(moduleConfig);
+
 
 export {
     createElement,
@@ -31,18 +52,3 @@ export {
     Component,
     Spec,
 };
-
-const createElement = React.createElement;
-
-function render(content, targetNode) {
-    if (!isElement(content)) {
-        throw new TypeError(
-            "[render] First argument 'content' has to be a valid element");
-    }
-
-    if (typeof targetNode === 'string') {
-        targetNode = document.getElementById(targetNode);
-    }
-
-    return ReactDOM.render(content, targetNode);
-}
