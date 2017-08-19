c61e63c28364bd9c3d4eb9cc15c8acdc8d957c7f
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2017-08-19 17:55:30 +0200

    Some updates

--- src/main/internal/constant/constants.js
+++ src/main/internal/constant/constants.js
@@ -8,7 +8,7 @@ const
         'constructor', 'forceUpdate']),
 
     REGEX_COMPONENT_SYSTEM_NAME = /^[a-z][a-z]*(-[a-z][a-z]*)*$/,
-    REGEX_COMPONENT_NAME = /^[A-Z][a-zA-Z0-9_\.]*$/,
+    REGEX_COMPONENT_DISPLAY_NAME = /^[A-Z][a-zA-Z0-9_\.]*$/,
     REGEX_PROPERTY_NAME = /^[a-z][a-zA-Z0-9_-]*$/,
     REGEX_INJECTION_NAME = /^[a-zA-Z][a-zA-Z0-9_-]*$/,
     REGEX_METHOD_NAME = /^[a-z][a-zA-Z0-9_-]*$/,
@@ -17,7 +17,7 @@ const
 export {
     FORBIDDEN_METHOD_NAMES,
     REGEX_COMPONENT_SYSTEM_NAME,
-    REGEX_COMPONENT_NAME,
+    REGEX_COMPONENT_DISPLAY_NAME,
     REGEX_PROPERTY_NAME,
     REGEX_INJECTION_NAME,
     REGEX_METHOD_NAME,
