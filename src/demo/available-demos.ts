a875b7ecfccc510eea4beeb4155a4d227af292d9
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-01-09 07:57:06 +0100

    Some updates

--- src/demo/available-demos.ts
+++ src/demo/available-demos.ts
@@ -3,12 +3,14 @@ import helloWorld from './demos/hello-world'
 import simpleCounter from './demos/simple-counter'
 import simpleCounterAlt from './demos/simple-counter-alt'
 import i18n from './demos/i18n'
+import iterators from './demos/iterators'
 
 const demos: [string, VirtualElement][] = [
   ['Hello world', helloWorld],
   ['Simple counter', simpleCounter],
   ['Simple counter (alternative)', simpleCounterAlt],
-  ['Internatinalization', i18n]
+  ['Internatinalization', i18n],
+  ['Iterators', iterators]
 ]
 
 export default demos
---
+++ src/demo/demos/iterators.tsx
@@ -0,0 +1,54 @@
+/* @jsx createElement */
+import { createElement, defineComponent } from 'js-surface'
+
+const demoContent = {
+  [Symbol.iterator]: function * () {
+    yield 'I'
+    yield 't'
+    yield 'e'
+    yield 'r'
+    yield 'a'
+    yield 't'
+    yield 'o'
+    yield 'r'
+    yield 's'
+
+    yield {
+      [Symbol.iterator]: function * () {
+        yield ' '
+        yield 'seem'
+        yield ' '
+        yield 'to'
+        yield ' '
+      }
+    }
+
+    yield 'w'
+    yield 'o'
+    yield ['r', 'k', ' ', 'p', {
+      [Symbol.iterator]: function * () {
+        yield 'r'
+        yield 'operly!'
+      }
+    }]
+  }
+}
+
+const App = defineComponent({
+  displayName:  'App',
+
+  render() {
+    return (
+      <div>
+        <div>
+            If everything works fine then the following line shall be:
+            "<i>Iterators seem to work properly!</i>"
+        </div>
+        <br/>
+        <div>&gt;&gt; {demoContent}</div>
+      </div>
+    )
+  }
+})
+
+export default App()
